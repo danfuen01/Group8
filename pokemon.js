e09c658c1f9364ac9be49123a95e60b1763ed777
tolander98 <tolander98@gmail.com>
2016-08-01 17:25:12 -0500

    idk what the changes were. But it still kinda works. #animation #respawn

--- pokemon.js
+++ pokemon.js
@@ -2,6 +2,7 @@
 
 function setupHandlers() {
 	pokemonSpawn();
+	checkHit();
 	document.body.onkeyup = function(e){
     	if (e.keyCode === 32) {//if spacebar is pushed
 
@@ -12,19 +13,19 @@ function setupHandlers() {
 	}
 }
 
-// function checkHit(){
-// 	console.log("Called overlap")
-// 	var rect1 {x: 0, y :550, width:13%, height: 18%}
-// 	var rect2 = {x: 20, y: 550, width: 5%, height: 7%}
-// 	var hit = false;
-// 	if (rect1.x < rect2.x + rect2.width &&
-//    rect1.x + rect1.width > rect2.x &&
-//    rect1.y < rect2.y + rect2.height &&
-//    rect1.height + rect1.y > rect2.y) {
-//         hit = true;
-//     	console.log("HIT")
-// }
-// }
+function checkHit(){
+	console.log("Called overlap")
+	var rect1= {x: 0, y :550, width:50, height: 50}
+	var rect2 = {x: 0, y: 550, width: 20, height: 20}
+	var hit = false;
+	if (rect1.x < rect2.x + rect2.width &&
+   rect1.x + rect1.width > rect2.x &&
+   rect1.y < rect2.y + rect2.height &&
+   rect1.height + rect1.y > rect2.y) {
+        hit = true;
+    	console.log("HIT")
+}
+}
 
 
 	function pokeballFire(){
@@ -45,21 +46,21 @@ function setupHandlers() {
 	}
 
 function pokemonSpawn(){
-	var pokemonIds = ["squirtle", "charmander", "bulbasaur"];
-	var thisID = pokemonIds[Math.floor(Math.random() * (4))];//selects a random pokemon
-	console.log(thisID);
-	var pokemon = document.getElementById(pokemonIds);
-	var pos = 1000;
+	// var pokemonIds = ["squirtle", "charmander", "bulbasaur"];
+	// var thisID = pokemonIds[Math.floor(Math.random() * (4))];//selects a random pokemon
+	// console.log(thisID);
+	var pokemon = document.getElementById("bulbasaur");
+	var pos = 60;
 	var id = setInterval(frame,5);
 	function frame() {
-		document.getElementById(thisID).className = 'show';//pokemon appears
+		document.getElementById("bulbasaur").className = 'show';//pokemon appears
 		
-		if(pos == 0){
+		if(pos == 1450){
 			clearInterval(id);
 
 		}else{
-			pos--;
-			document.getElementById(thisID).style.right = pos + 'px';//change postion
+			pos++;
+			document.getElementById("bulbasaur").style.right = pos + 'px';//change postion
 		}
 	}
 
